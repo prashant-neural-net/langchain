54e25741535b641da9380d2b8b5103fe7ca237ca
Prashant Tripathi <prashant-neural-net@gmail.com>
2026-09-08 19:37:42 +0530

    iniatialization

---
+++ LLMs/llm_demo.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+
+load_dotenv()
+
+client = OpenAI(base_url=os.environ["Base_Url"], api_key="ollama")
+
+response = client.chat.completions.create(
+    model="llama3.2:3b",
+    messages=[{"role": "user", "content": "what is dependency injection in fastapi"}],
+    stream=True,
+    
+)
+
+
+for chunk in response:
+    if chunk:
+        content = chunk.choices[0].delta.content
+        if content:
+            print(content, end="", flush=True)
+print()
